e7131e797c996973d127282879e0f8d103da01f0
Taiki <tyoshikawa1106@gmail.com>
2015-11-15 21:37:45 +0900

    commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,28 +1,19 @@
 package com.example;
 
-import com.example.app.Calculator;
+import com.example.app.Frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
-import java.util.Scanner;
-
 @EnableAutoConfiguration
-@Import(AppConfig.class)
+@ComponentScan
 public class App {
     public static void main(String[] args) {
         try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter 2 numbers like 'a b' : ");
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-
-            Calculator calculator = context.getBean(Calculator.class);
-            int result = calculator.calc(a, b);
-
-            System.out.println("result = " + result);
+            Frontend frontend = context.getBean(Frontend.class);
+            frontend.run();
         }
     }
 }
